0d282412c8842cd373225a1fa9f1f1a22a69031f
evandro <ebolzan@inf.ufsm.br>
2021-09-10 15:22:26 -0300

    second file

---
+++ program2.py
@@ -0,0 +1,12 @@
+#solution of problem 2 way
+
+#1 hour 01:11 -> 4 digits 
+#1 hour 4 digits * 60 = 240 
+#24 hours (240 * 23)/2 = 2760 values
+# 
+
+HOUR   = 23
+MIN    = 60
+DIGITS = 4
+
+print("One hour 00:22 (4 digits) * 60 * (23) / 2 ", (DIGITS*MIN*HOUR)/2, end="END.") 
